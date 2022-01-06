e6b04df0273d5df97919c6c18ee247da36a6ca50
tonyban <95233444+tonyban@users.noreply.github.com>
2022-01-07 02:18:18 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -4,40 +4,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
-let [따봉, 따봉변경] = useState(0);
-let [modal, modal변경] = useState(false); // 모달창을 켜고 닫는 스위치
-let posts = '강남 고기 맛집';
-function 제목바꾸기() {
-  var newArray = [...글제목];
-  newArray[0] = '여자코트 추천';
-  글제목변경(newArray);
-}
+let [title, titleChange] = useState(['자바스크립트 독학법', '코딩부트캠프 추천', '비전공자 개발자 현황']);
+let [like, likeChange] = useState(0);
+let [modal, modalChange] = useState(true); // 모달창을 켜고 닫는 스위치
+let [secondTitle, secondTitleChange] = useState(0);
+let [inputValue, inputValueChange] = useState('');
 
 return (
    <div className="App">
      <div className="black-nav">
-       <div>개발 Blog</div>
+       <h1>삼빛나 선익의 개발 Blog</h1>
      </div>
 
      {  //이렇게 map을 이용하면 위의 글제목 태그들 모두 지워도 됨
-       글제목.map(function(글){
+       title.map(function(title, i){
          return (
          <div className="list">
-          <h3> { 글 } <span onClick={()=>{ 따봉변경(따봉 + 1)}}>👍</span> {따봉} 
+          <h3 onClick={()=>{ secondTitle(i) }}> { title } <span onClick={()=>{ likeChange(like + 1)}}>👍</span> {like} 
           </h3>
-           <p>2월 18일 발행</p>
+           <p>2022년 1월 2일 발행</p>
            <hr/>
          </div>  
          )
        })
      }
 
-     <button onClick={()=>{mpdal변경(!modal)}}>열고닫기</button>
+     {/* <button onClick={()=>{ 누른제목변경(0) }}>버튼1</button>
+     <button onClick={()=>{ 누른제목변경(1) }}>버튼2</button>
+     <button onClick={()=>{ 누른제목변경(2) }}>버튼3</button> */}
+   
+     <div className="publish">
+       <input onChange={(e)=>{inputValueChange(e.target.value)}}/>
+       <button onClick={()=>{
+         var arrayCopy = [...title];
+         arrayCopy.unshift(inputValue);
+         titleChange(arrayCopy);
+       }}>저장</button>
+     </div>
+
+     <button onClick={()=>{modalChange(!modal)}}>열고닫기</button>
 
      {
        modal === true  //3항연산, 만약 모달창이 true라면
-       ? <Modal></Modal>
+       ? <Modal title={title} secondTitle={secondTitle}></Modal>
        : null
      }
 
@@ -45,10 +54,10 @@ return (
   );
 }
 
-function Modal () {
+function Modal(props){
   return (
     <div className="modal">
-       <h2>제목</h2>
+       <h2> {props.title[props.secondTitle]} </h2>
        <p>날짜</p>
        <p>상세내용</p>   
     </div>
